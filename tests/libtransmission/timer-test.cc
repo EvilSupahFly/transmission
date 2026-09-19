48ff0e9d218a549a1c66ddb14e96134d3256e30c
EvilSupahFly <seann.giffin@gmail.com>
2026-09-18 20:22:58 -0400

    Update timer-test.cc
    Reverted typo in tests/libtransmission/timer-test.cc

--- tests/libtransmission/timer-test.cc
+++ tests/libtransmission/timer-test.cc
@@ -143,7 +143,7 @@ TEST_F(TimerTest, singleShotHonorsInterval)
 
     // confirm that it kicked at the right interval
     EXPECT_TRUE(called);
-    expectSingleShotInterval(Interval, AsMSec(end_time - begin_time));
+    expectInterval(Interval, AsMSec(end_time - begin_time));
 }
 
 TEST_F(TimerTest, repeatingHonorsInterval)
